64c01e1723c7c7312b793172c277e34f2357fb34
accawsdevops <acc.aws.devops@gmail.com>
2025-05-19 19:36:39 +0530

    updating the code-build-deploy

--- script/install.sh
+++ script/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo systemctl stop nginx
+sudo systemctl stop nginx   # It will stop the Nginx
 echo "Clearing old build files..."
-sudo rm -rf /usr/share/nginx/html/*
+sudo rm -rf /usr/share/nginx/html/* # Remove old app files from nginx
 
 echo "Copying new build files to Nginx directory..."
-sudo cp -r /home/ec2-user/nodejsbuild/build/* /usr/share/nginx/html/
+sudo cp -r /home/ec2-user/nodejsbuild/build/* /usr/share/nginx/html/    #copy new app file to nginx
 
 echo "Restarting Nginx..."
 sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo systemctl enable nginx #restart nginx
